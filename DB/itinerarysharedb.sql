9303139d7f594c080fc0a038376f96e7fa2f347b
Carlos Lobera <carlosclobera@gmail.com>
2022-12-22 17:11:12 -0600

    swap erd

--- DB/itinerarysharedb.sql
+++ DB/itinerarysharedb.sql
@@ -5,6 +5,9 @@ SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
 
 -- -----------------------------------------------------
+-- Schema mydb
+-- -----------------------------------------------------
+-- -----------------------------------------------------
 -- Schema itinerarysharedb
 -- -----------------------------------------------------
 DROP SCHEMA IF EXISTS `itinerarysharedb` ;
@@ -21,15 +24,20 @@ USE `itinerarysharedb` ;
 DROP TABLE IF EXISTS `user` ;
 
 CREATE TABLE IF NOT EXISTS `user` (
-  `id` INT NOT NULL AUTO_INCREMENT,
+  `id` INT(11) NOT NULL AUTO_INCREMENT,
   `username` VARCHAR(45) NOT NULL,
   `password` VARCHAR(45) NOT NULL,
-  `enabled` TINYINT NULL DEFAULT 1,
-  `role` VARCHAR(45) NULL,
-  `profile_picture` VARCHAR(2048) NULL,
+  `enabled` TINYINT(4) NULL DEFAULT '1',
+  `role` VARCHAR(45) NULL DEFAULT NULL,
+  `profile_picture` VARCHAR(2048) NULL DEFAULT NULL,
+  `first_name` VARCHAR(100) NULL,
+  `last_name` VARCHAR(100) NULL,
+  `biography` TEXT NULL,
   PRIMARY KEY (`id`),
   UNIQUE INDEX `username_UNIQUE` (`username` ASC))
-ENGINE = InnoDB;
+ENGINE = InnoDB
+AUTO_INCREMENT = 2
+DEFAULT CHARACTER SET = utf8;
 
 
 -- -----------------------------------------------------
@@ -38,17 +46,15 @@ ENGINE = InnoDB;
 DROP TABLE IF EXISTS `itinerary` ;
 
 CREATE TABLE IF NOT EXISTS `itinerary` (
-  `id` INT NOT NULL,
-  `active` TINYINT NOT NULL DEFAULT 1,
-  `country` VARCHAR(45) NOT NULL,
-  `state` VARCHAR(45) NULL,
-  `city` VARCHAR(100) NOT NULL,
-  `budget` DECIMAL(6,2) NULL,
-  `start_date` DATE NULL,
-  `end_date` DATE NULL,
+  `id` INT(11) NOT NULL,
+  `active` TINYINT(4) NOT NULL DEFAULT '1',
+  `budget` DECIMAL(6,2) NULL DEFAULT NULL,
+  `start_date` DATE NULL DEFAULT NULL,
+  `end_date` DATE NULL DEFAULT NULL,
   `image` VARCHAR(2048) NULL DEFAULT 'https://media.istockphoto.com/photos/couple-relax-on-the-beach-enjoy-beautiful-sea-on-the-tropical-island-picture-id1160947136?b=1&k=20&m=1160947136&s=612x612&w=0&h=AsFmKSBYTtacl0DvI-RanCnAXFU0cmuW8NAo0g-tGzA=',
-  `user_id` INT NOT NULL,
-  `rating` INT UNSIGNED NOT NULL DEFAULT 0,
+  `user_id` INT(11) NOT NULL,
+  `name` VARCHAR(100) NOT NULL DEFAULT 'My Itinerary',
+  `description` TEXT NULL,
   PRIMARY KEY (`id`),
   INDEX `fk_itinerary_user1_idx` (`user_id` ASC),
   CONSTRAINT `fk_itinerary_user1`
@@ -56,78 +62,42 @@ CREATE TABLE IF NOT EXISTS `itinerary` (
     REFERENCES `user` (`id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
-ENGINE = InnoDB;
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8;
 
 
 -- -----------------------------------------------------
--- Table `accommodation`
+-- Table `itinerary_comment`
 -- -----------------------------------------------------
-DROP TABLE IF EXISTS `accommodation` ;
+DROP TABLE IF EXISTS `itinerary_comment` ;
 
-CREATE TABLE IF NOT EXISTS `accommodation` (
-  `id` INT NOT NULL AUTO_INCREMENT,
-  `type` VARCHAR(45) NOT NULL,
-  `rate` DECIMAL(6,2) NOT NULL,
-  `address` VARCHAR(100) NULL,
-  PRIMARY KEY (`id`))
-ENGINE = InnoDB;
-
-
--- -----------------------------------------------------
--- Table `rental_car`
--- -----------------------------------------------------
-DROP TABLE IF EXISTS `rental_car` ;
-
-CREATE TABLE IF NOT EXISTS `rental_car` (
-  `id` INT NOT NULL AUTO_INCREMENT,
-  `rate` DECIMAL(6,2) NOT NULL,
-  `make` VARCHAR(45) NULL,
-  `model` VARCHAR(45) NULL,
-  PRIMARY KEY (`id`))
-ENGINE = InnoDB;
-
-
--- -----------------------------------------------------
--- Table `flight`
--- -----------------------------------------------------
-DROP TABLE IF EXISTS `flight` ;
-
-CREATE TABLE IF NOT EXISTS `flight` (
-  `id` INT NOT NULL,
-  `date` DATE NOT NULL,
-  `price` DECIMAL(6,2) NOT NULL,
-  `airline` VARCHAR(45) NULL,
-  PRIMARY KEY (`id`))
-ENGINE = InnoDB;
-
-
--- -----------------------------------------------------
--- Table `dining`
--- -----------------------------------------------------
-DROP TABLE IF EXISTS `dining` ;
-
-CREATE TABLE IF NOT EXISTS `dining` (
-  `id` INT NOT NULL AUTO_INCREMENT,
-  `name` VARCHAR(45) NOT NULL,
-  `type` VARCHAR(45) NULL,
-  `reservation_required` TINYINT NULL,
-  PRIMARY KEY (`id`))
-ENGINE = InnoDB;
-
-
--- -----------------------------------------------------
--- Table `event`
--- -----------------------------------------------------
-DROP TABLE IF EXISTS `event` ;
-
-CREATE TABLE IF NOT EXISTS `event` (
-  `id` INT NOT NULL AUTO_INCREMENT,
-  `name` VARCHAR(45) NOT NULL,
-  `date` DATE NOT NULL,
-  `type` VARCHAR(45) NULL,
-  `price` DECIMAL(6,2) NULL,
-  PRIMARY KEY (`id`))
-ENGINE = InnoDB;
+CREATE TABLE IF NOT EXISTS `itinerary_comment` (
+  `id` INT(11) NOT NULL AUTO_INCREMENT,
+  `user_id` INT(11) NOT NULL,
+  `post` TEXT NOT NULL,
+  `reply` INT(11) NULL DEFAULT NULL,
+  `itinerary_id` INT(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  INDEX `fk_user_id_idx` (`user_id` ASC),
+  INDEX `fk_reply_id_idx` (`reply` ASC),
+  INDEX `fk_comment_itinerary1_idx` (`itinerary_id` ASC),
+  CONSTRAINT `fk_comment_itinerary`
+    FOREIGN KEY (`itinerary_id`)
+    REFERENCES `itinerary` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_reply_id`
+    FOREIGN KEY (`reply`)
+    REFERENCES `itinerary_comment` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_user_id`
+    FOREIGN KEY (`user_id`)
+    REFERENCES `user` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8;
 
 
 -- -----------------------------------------------------
@@ -136,122 +106,42 @@ ENGINE = InnoDB;
 DROP TABLE IF EXISTS `destination` ;
 
 CREATE TABLE IF NOT EXISTS `destination` (
-  `id` INT NOT NULL,
-  `itinerary_id` INT NOT NULL,
-  `previous_destination` INT NULL,
-  `next_destination` INT NULL,
-  `accommodation_id` INT NULL,
-  `event_id` INT NULL,
-  `flight_id` INT NULL,
-  `dining_id` INT NULL,
-  `rental_car_id` INT NULL,
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `name` VARCHAR(100) NOT NULL,
+  `description` TEXT NULL,
+  `image` TEXT NULL,
+  `country` VARCHAR(100) NULL,
+  `city` VARCHAR(100) NULL,
+  PRIMARY KEY (`id`))
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `itinerary_item`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `itinerary_item` ;
+
+CREATE TABLE IF NOT EXISTS `itinerary_item` (
+  `id` INT(11) NOT NULL,
+  `itinerary_id` INT(11) NOT NULL,
+  `trip_day` INT NOT NULL DEFAULT 0,
+  `destination_id` INT NOT NULL,
+  `description` TEXT NULL,
   PRIMARY KEY (`id`),
-  INDEX `fk_event_id_idx` (`event_id` ASC),
-  INDEX `fk_accomodation_id_idx` (`accommodation_id` ASC),
-  INDEX `fk_flight_id_idx` (`flight_id` ASC),
-  INDEX `fk_dining_id_idx` (`dining_id` ASC),
-  INDEX `fk_rental_car_id_idx` (`rental_car_id` ASC),
-  INDEX `fk_previous_destination_id_idx` (`previous_destination` ASC),
-  INDEX `fk_next_destination_id_idx` (`next_destination` ASC),
   INDEX `fk_itinerary_id_idx` (`itinerary_id` ASC),
-  CONSTRAINT `fk_event_id`
-    FOREIGN KEY (`event_id`)
-    REFERENCES `event` (`id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
-  CONSTRAINT `fk_accomodation_id`
-    FOREIGN KEY (`accommodation_id`)
-    REFERENCES `accommodation` (`id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
-  CONSTRAINT `fk_flight_id`
-    FOREIGN KEY (`flight_id`)
-    REFERENCES `flight` (`id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
-  CONSTRAINT `fk_dining_id`
-    FOREIGN KEY (`dining_id`)
-    REFERENCES `dining` (`id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
-  CONSTRAINT `fk_rental_car_id`
-    FOREIGN KEY (`rental_car_id`)
-    REFERENCES `rental_car` (`id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
-  CONSTRAINT `fk_previous_destination_id`
-    FOREIGN KEY (`previous_destination`)
-    REFERENCES `destination` (`id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
-  CONSTRAINT `fk_next_destination_id`
-    FOREIGN KEY (`next_destination`)
-    REFERENCES `destination` (`id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
+  INDEX `fk_itinerary_item_destination1_idx` (`destination_id` ASC),
   CONSTRAINT `fk_itinerary_id`
     FOREIGN KEY (`itinerary_id`)
     REFERENCES `itinerary` (`id`)
     ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB;
-
-
--- -----------------------------------------------------
--- Table `user_has_itinerary`
--- -----------------------------------------------------
-DROP TABLE IF EXISTS `user_has_itinerary` ;
-
-CREATE TABLE IF NOT EXISTS `user_has_itinerary` (
-  `user_id` INT NOT NULL,
-  `itinerary_id` INT NOT NULL,
-  PRIMARY KEY (`user_id`, `itinerary_id`),
-  INDEX `fk_user_has_itinerary_itinerary1_idx` (`itinerary_id` ASC),
-  INDEX `fk_user_has_itinerary_user1_idx` (`user_id` ASC),
-  CONSTRAINT `fk_user_has_itinerary_user1`
-    FOREIGN KEY (`user_id`)
-    REFERENCES `user` (`id`)
-    ON DELETE NO ACTION
     ON UPDATE NO ACTION,
-  CONSTRAINT `fk_user_has_itinerary_itinerary1`
-    FOREIGN KEY (`itinerary_id`)
-    REFERENCES `itinerary` (`id`)
+  CONSTRAINT `fk_itinerary_item_destination1`
+    FOREIGN KEY (`destination_id`)
+    REFERENCES `destination` (`id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
-ENGINE = InnoDB;
-
-
--- -----------------------------------------------------
--- Table `comment`
--- -----------------------------------------------------
-DROP TABLE IF EXISTS `comment` ;
-
-CREATE TABLE IF NOT EXISTS `comment` (
-  `id` INT NOT NULL AUTO_INCREMENT,
-  `user_id` INT NOT NULL,
-  `post` TEXT NOT NULL,
-  `reply` INT NULL,
-  `itinerary_id` INT NOT NULL,
-  PRIMARY KEY (`id`),
-  INDEX `fk_user_id_idx` (`user_id` ASC),
-  INDEX `fk_reply_id_idx` (`reply` ASC),
-  INDEX `fk_comment_itinerary1_idx` (`itinerary_id` ASC),
-  CONSTRAINT `fk_user_id`
-    FOREIGN KEY (`user_id`)
-    REFERENCES `user` (`id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
-  CONSTRAINT `fk_reply_id`
-    FOREIGN KEY (`reply`)
-    REFERENCES `comment` (`id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
-  CONSTRAINT `fk_comment_itinerary`
-    FOREIGN KEY (`itinerary_id`)
-    REFERENCES `itinerary` (`id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB;
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8;
 
 
 -- -----------------------------------------------------
@@ -260,32 +150,168 @@ ENGINE = InnoDB;
 DROP TABLE IF EXISTS `review` ;
 
 CREATE TABLE IF NOT EXISTS `review` (
-  `id` INT NOT NULL,
-  `comment` TEXT NULL,
-  `rating` INT(1) UNSIGNED NOT NULL DEFAULT 0,
-  `user_id` INT NOT NULL,
-  `destination_id` INT NOT NULL,
+  `id` INT(11) NOT NULL,
+  `comment` TEXT NULL DEFAULT NULL,
+  `rating` INT(1) UNSIGNED NOT NULL DEFAULT '0',
+  `user_id` INT(11) NOT NULL,
+  `destination_id` INT(11) NOT NULL,
   PRIMARY KEY (`id`),
   INDEX `fk_review_user1_idx` (`user_id` ASC),
   INDEX `fk_review_destination1_idx` (`destination_id` ASC),
+  CONSTRAINT `fk_review_destination1`
+    FOREIGN KEY (`destination_id`)
+    REFERENCES `itinerary_item` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
   CONSTRAINT `fk_review_user1`
     FOREIGN KEY (`user_id`)
     REFERENCES `user` (`id`)
     ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8;
+
+
+-- -----------------------------------------------------
+-- Table `user_has_bookmarked_itinerary`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `user_has_bookmarked_itinerary` ;
+
+CREATE TABLE IF NOT EXISTS `user_has_bookmarked_itinerary` (
+  `user_id` INT(11) NOT NULL,
+  `itinerary_id` INT(11) NOT NULL,
+  PRIMARY KEY (`user_id`, `itinerary_id`),
+  INDEX `fk_user_has_itinerary_itinerary1_idx` (`itinerary_id` ASC),
+  INDEX `fk_user_has_itinerary_user1_idx` (`user_id` ASC),
+  CONSTRAINT `fk_user_has_itinerary_itinerary1`
+    FOREIGN KEY (`itinerary_id`)
+    REFERENCES `itinerary` (`id`)
+    ON DELETE NO ACTION
     ON UPDATE NO ACTION,
-  CONSTRAINT `fk_review_destination1`
-    FOREIGN KEY (`destination_id`)
-    REFERENCES `destination` (`id`)
+  CONSTRAINT `fk_user_has_itinerary_user1`
+    FOREIGN KEY (`user_id`)
+    REFERENCES `user` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8;
+
+
+-- -----------------------------------------------------
+-- Table `travel_detail_type`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `travel_detail_type` ;
+
+CREATE TABLE IF NOT EXISTS `travel_detail_type` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `name` VARCHAR(100) NOT NULL,
+  PRIMARY KEY (`id`))
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `travel_detail`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `travel_detail` ;
+
+CREATE TABLE IF NOT EXISTS `travel_detail` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `description` TEXT NOT NULL,
+  `itinerary_item_id` INT(11) NOT NULL,
+  `travel_detail_type_id` INT NOT NULL,
+  PRIMARY KEY (`id`),
+  INDEX `fk_travel_detail_itinerary_item1_idx` (`itinerary_item_id` ASC),
+  INDEX `fk_travel_detail_travel_detail_type1_idx` (`travel_detail_type_id` ASC),
+  CONSTRAINT `fk_travel_detail_itinerary_item1`
+    FOREIGN KEY (`itinerary_item_id`)
+    REFERENCES `itinerary_item` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_travel_detail_travel_detail_type1`
+    FOREIGN KEY (`travel_detail_type_id`)
+    REFERENCES `travel_detail_type` (`id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
 
-SET SQL_MODE = '';
-DROP USER IF EXISTS itdbadmin@localhost;
-SET SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
-CREATE USER 'itdbadmin'@'localhost' IDENTIFIED BY 'itdbadmin';
 
-GRANT SELECT, INSERT, TRIGGER, UPDATE, DELETE ON TABLE * TO 'itdbadmin'@'localhost';
+-- -----------------------------------------------------
+-- Table `destination_comment`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `destination_comment` ;
+
+CREATE TABLE IF NOT EXISTS `destination_comment` (
+  `id` INT(11) NOT NULL AUTO_INCREMENT,
+  `user_id` INT(11) NOT NULL,
+  `post` TEXT NOT NULL,
+  `reply` INT(11) NULL DEFAULT NULL,
+  `destination_id` INT NOT NULL,
+  PRIMARY KEY (`id`),
+  INDEX `fk_user_id_idx` (`user_id` ASC),
+  INDEX `fk_reply_id_idx` (`reply` ASC),
+  INDEX `fk_destination_comment_destination1_idx` (`destination_id` ASC),
+  CONSTRAINT `fk_reply_id0`
+    FOREIGN KEY (`reply`)
+    REFERENCES `destination_comment` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_user_id0`
+    FOREIGN KEY (`user_id`)
+    REFERENCES `user` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_destination_comment_destination1`
+    FOREIGN KEY (`destination_id`)
+    REFERENCES `destination` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8;
+
+
+-- -----------------------------------------------------
+-- Table `destination_rating`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `destination_rating` ;
+
+CREATE TABLE IF NOT EXISTS `destination_rating` (
+  `rating` INT NOT NULL,
+  `rating_comment` TEXT NULL,
+  `destination_id` INT NOT NULL,
+  `user_id` INT(11) NOT NULL,
+  PRIMARY KEY (`destination_id`, `user_id`),
+  INDEX `fk_destination_rating_user1_idx` (`user_id` ASC),
+  CONSTRAINT `fk_destination_rating_destination1`
+    FOREIGN KEY (`destination_id`)
+    REFERENCES `destination` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_destination_rating_user1`
+    FOREIGN KEY (`user_id`)
+    REFERENCES `user` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `trip_picture`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `trip_picture` ;
+
+CREATE TABLE IF NOT EXISTS `trip_picture` (
+  `id` INT NOT NULL,
+  `image_url` VARCHAR(2048) NOT NULL,
+  `itinerary_item_id` INT(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  INDEX `fk_trip_picture_itinerary_item1_idx` (`itinerary_item_id` ASC),
+  CONSTRAINT `fk_trip_picture_itinerary_item1`
+    FOREIGN KEY (`itinerary_item_id`)
+    REFERENCES `itinerary_item` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
 
 SET SQL_MODE=@OLD_SQL_MODE;
 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
@@ -296,7 +322,51 @@ SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
 -- -----------------------------------------------------
 START TRANSACTION;
 USE `itinerarysharedb`;
-INSERT INTO `user` (`id`, `username`, `password`, `enabled`, `role`, `profile_picture`) VALUES (1, 'admin', 'chaotic_slime', 1, '', 'ADMIN');
+INSERT INTO `user` (`id`, `username`, `password`, `enabled`, `role`, `profile_picture`, `first_name`, `last_name`, `biography`) VALUES (1, 'admin', 'chaotic_slime', 1, 'ADMIN', NULL, NULL, NULL, NULL);
+
+COMMIT;
+
+
+-- -----------------------------------------------------
+-- Data for table `itinerary`
+-- -----------------------------------------------------
+START TRANSACTION;
+USE `itinerarysharedb`;
+INSERT INTO `itinerary` (`id`, `active`, `budget`, `start_date`, `end_date`, `image`, `user_id`, `name`, `description`) VALUES (1, 1, 2000.00, '2022-12-25', '2022-12-30', NULL, 1, DEFAULT, NULL);
+
+COMMIT;
+
+
+-- -----------------------------------------------------
+-- Data for table `itinerary_comment`
+-- -----------------------------------------------------
+START TRANSACTION;
+USE `itinerarysharedb`;
+INSERT INTO `itinerary_comment` (`id`, `user_id`, `post`, `reply`, `itinerary_id`) VALUES (1, 1, 'This trip was great', NULL, 1);
+
+COMMIT;
+
+
+-- -----------------------------------------------------
+-- Data for table `user_has_bookmarked_itinerary`
+-- -----------------------------------------------------
+START TRANSACTION;
+USE `itinerarysharedb`;
+INSERT INTO `user_has_bookmarked_itinerary` (`user_id`, `itinerary_id`) VALUES (1, 1);
+
+COMMIT;
+
+
+-- -----------------------------------------------------
+-- Data for table `travel_detail_type`
+-- -----------------------------------------------------
+START TRANSACTION;
+USE `itinerarysharedb`;
+INSERT INTO `travel_detail_type` (`id`, `name`) VALUES (1, 'flight');
+INSERT INTO `travel_detail_type` (`id`, `name`) VALUES (2, 'train');
+INSERT INTO `travel_detail_type` (`id`, `name`) VALUES (3, 'rental car');
+INSERT INTO `travel_detail_type` (`id`, `name`) VALUES (4, 'accommodation');
+INSERT INTO `travel_detail_type` (`id`, `name`) VALUES (5, 'dining');
 
 COMMIT;
 
