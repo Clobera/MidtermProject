308d1391b18bf6565322c7e2ef32e7c619e364b2
Ryan Goode <goode21@gmail.com>
2023-01-04 11:30:46 -0700

    peristence XML updated with all entities

--- JPAItineraryShare/src/main/java/com/skilldistillery/itinerary/entities/ItineraryComment.java
+++ JPAItineraryShare/src/main/java/com/skilldistillery/itinerary/entities/ItineraryComment.java
@@ -2,13 +2,16 @@ package com.skilldistillery.itinerary.entities;
 
 import java.util.Objects;
 
+import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 
+@Entity
 public class ItineraryComment {
+	
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private int id;
