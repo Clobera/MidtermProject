6d8b8e6b9f5a4ed9696a6737d1bdf636d6aa2492
Carlos Lobera <carlosclobera@gmail.com>
2023-01-06 15:24:26 -0600

    update controller, and navbar

--- ItineraryShare/src/main/java/com/skilldistillery/itinerary/controllers/ItineraryController.java
+++ ItineraryShare/src/main/java/com/skilldistillery/itinerary/controllers/ItineraryController.java
@@ -9,8 +9,11 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.SessionAttributes;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import com.skilldistillery.itinerary.data.DestinationDAO;
 import com.skilldistillery.itinerary.data.ItineraryDAO;
+import com.skilldistillery.itinerary.entities.Destination;
 import com.skilldistillery.itinerary.entities.Itinerary;
 import com.skilldistillery.itinerary.entities.User;
 
@@ -21,12 +24,42 @@ public class ItineraryController {
 	@Autowired
 	private ItineraryDAO itineraryDao;
 	
+	@Autowired
+	private DestinationDAO destinationDao;
+
 	@ModelAttribute("loggedInUser")
-	public User initSessionState () {
+	public User initSessionState() {
 		return new User();
 	}
-	
-	@GetMapping(path = "goCreateItinerary.do" )
+
+	@GetMapping(path = "viewItinerary.do")
+	public String viewItinerary(Model model, Integer id) {
+		Itinerary showItinerary = itineraryDao.findItinerary(id);
+		model.addAttribute("itinerary", showItinerary);
+		return "itinerary";
+	}
+
+	@GetMapping(path = "goCreateDestination.do")
+	public String goCreateDestination(Model model, Integer itineraryId) {
+		model.addAttribute("itineraryId", itineraryId);
+		return "createDestination";
+	}
+
+	@PostMapping(path = "createDestination.do")
+	public String createdDestination(Model model, Integer itineraryId, Destination destination, RedirectAttributes redir) {
+		destinationDao.createDestination(destination);
+		String view = "redirect:viewItinerary.do";
+		if(itineraryId == null) {
+			view = "home";
+			List<Itinerary> itineraries = itineraryDao.findAllActiveItineraries();
+			model.addAttribute("itineraries", itineraries);
+		}else {
+			redir.addFlashAttribute("id", itineraryId);
+		}
+		return view;
+	}
+
+	@GetMapping(path = "goCreateItinerary.do")
 	public String goCreateItinerary(Model model, @ModelAttribute("loggedInUser") User user) {
 		String destination = "createItinerary";
 		if (user.getId() == 0) {
@@ -36,23 +69,8 @@ public class ItineraryController {
 		}
 		return destination;
 	}
-	
-	@GetMapping(path = "viewItinerary.do" )
-	public String viewItinerary(Model model, int id) {
-		Itinerary showItinerary = itineraryDao.findItinerary(id);
-		model.addAttribute("itinerary", showItinerary);
-		return "itinerary";
-	}
-	
-	@GetMapping(path = "goCreateDestination.do" )
-	public String goCreateDestination(Model model, int id, @ModelAttribute("loggedInUser") User user) {
-		Itinerary itinerary = itineraryDao.findItinerary(id);
-		model.addAttribute("itinerary", itinerary);
-		return "createDestination";
-	}
-	
-	
-	@PostMapping(path = "createItinerary.do" )
+
+	@PostMapping(path = "createItinerary.do")
 	public String createdItinerary(Itinerary itinerary, @ModelAttribute("loggedInUser") User user) {
 		itineraryDao.createItinerary(user, itinerary);
 		return "home";
