652b154c6181c31f3292d377a717a1db8cbe37cb
Forrest January <fjanuary9@gmail.com>
2023-01-06 17:34:07 -0600

    Git Merge

--- ItineraryShare/src/main/java/com/skilldistillery/itinerary/controllers/ItineraryItemController.java
+++ ItineraryShare/src/main/java/com/skilldistillery/itinerary/controllers/ItineraryItemController.java
@@ -1,16 +1,13 @@
 package com.skilldistillery.itinerary.controllers;
 
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.SessionAttributes;
 
-import com.skilldistillery.itinerary.entities.Itinerary;
+import com.skilldistillery.itinerary.data.ItineraryItemDAO;
 import com.skilldistillery.itinerary.entities.ItineraryItem;
 import com.skilldistillery.itinerary.entities.User;
 
